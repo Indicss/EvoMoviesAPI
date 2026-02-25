e91d981e93439c15b3a469fb314015f6311e9938
Bogdan K <bkalinovski@starnet.md>
2026-02-25 19:06:24 +0200

    add cors

--- src/EvoMovies.Api/Program.cs
+++ src/EvoMovies.Api/Program.cs
@@ -2,6 +2,8 @@ using EvoMovies.Api.Infrastructure.Persistence;
 using EvoMovies.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
+const string corsOrigin = "_evoMoviesOrigin";
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
@@ -9,10 +11,22 @@ var builder = WebApplication.CreateBuilder(args);
     
     builder.Services.AddDbContext<AppDbContext>(options => 
         options.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationDatabase")));
+    
+    var allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins")?.Split(',') ?? [];
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(corsOrigin, policy =>
+        {
+            policy.WithOrigins(allowedOrigins)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+    });
 }
 
 var app = builder.Build();
+app.UseCors(corsOrigin);
 app.UseHttpsRedirection();
-
 app.MapControllers();
+
 app.Run();
